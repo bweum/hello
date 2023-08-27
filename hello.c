e9c0c44d68c602a6a3c7b335b95ad1433035a187
bweum <bweum2@huskers.unl.edu>
2023-08-27 12:56:58 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Brock Weum
+ *
+ * Date 2023/08/27
+ *
+ * Hack #1
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Brock Weum\n");
+    
+}
